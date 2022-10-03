48ff5a3fa330858531e51e9702c4ab0878f6fc84
Berke YONCACI <berke5sekiz1@gmail.com>
2022-10-03 16:50:49 +0300

    fix file upload for heroku

--- src/middlewares/imageUpload.ts
+++ src/middlewares/imageUpload.ts
@@ -1,9 +1,16 @@
+import { createReadStream, unlinkSync } from 'fs'
 import path from 'path'
 import { NextFunction, Request, Response, RequestHandler } from 'express'
 import multer, { FileFilterCallback, MulterError } from 'multer'
 import mkdirp from 'mkdirp'
 import { v4 as uuidv4 } from 'uuid'
+import { ImgurClient } from 'imgur'
 import { combineMiddlewares } from '@src/utils/combineMiddlewares'
+import logger from '@src/logging'
+
+const imgur = new ImgurClient({
+  clientId: process.env.IMGUR_CLIENT_ID,
+})
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
@@ -38,14 +45,29 @@ const upload = multer({
   fileFilter,
 })
 
-function addImageInsideBody(req: Request, _res: Response, next: NextFunction) {
+async function addImageInsideBody(req: Request, res: Response, next: NextFunction) {
   const { file } = req
   if (!file) {
     return next()
   }
 
-  const fileField = { [file.fieldname]: file.path.replace(/\\/g, '/') }
-  req.body = { ...req.body, ...fileField }
+  try {
+    const filePath = file.path.replace(/\\/g, '/')
+
+    const response = await imgur.upload({
+      // TODO: fix any usage here
+      image: createReadStream(filePath) as any,
+      type: 'stream',
+    })
+
+    const fileField = { [file.fieldname]: response.data.link }
+    req.body = { ...req.body, ...fileField }
+
+    unlinkSync(filePath)
+  } catch (err) {
+    logger.error(err)
+    return res.status(500).json({ message: 'An error has occurred.' }) // TODO: handle JSON errors in a more generic way
+  }
 
   next()
 }
